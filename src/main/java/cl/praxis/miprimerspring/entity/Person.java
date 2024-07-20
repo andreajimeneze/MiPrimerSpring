59e7769d026057252735e0740e3ee31f21f638a5
Andrea Jiménez <andreacjimenez@gmail.com>
2024-07-20 20:42:14 -0300

    primer commit

---
+++ src/main/java/cl/praxis/miprimerspring/entity/Person.java
@@ -0,0 +1,14 @@
+package cl.praxis.miprimerspring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+}
